6880584345aa1fda4d9da0ff9ffe552eb3c26a36
felipebigarellidantasdacosta <felipe.costa@g4tech.com.br>
2021-12-15 16:40:57 -0300

    API CRUD with only JS

---
+++ src/index.js
@@ -0,0 +1,45 @@
+const http = require('http');
+const { URL } = require('url');
+
+const bodyParser = require('./helpers/bodyParser'); 
+const routes = require('./routes');
+
+const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http://localhost:3333${request.url}`);
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
+
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndpoint = pathname.split('/').filter(Boolean);
+
+  if (splitEndpoint.length > 1) {
+    pathname = `/${splitEndpoint[0]}/:id`; 
+    id = splitEndpoint[1];
+  }
+  
+  const route = routes.find((routeObj) => (
+    routeObj.endpoint === pathname && routeObj.method === request.method
+  ));
+
+  if (route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.params = { id };
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, { 'Content-Type': 'application/json' });
+      response.end(JSON.stringify(body));
+    };
+    
+    if (['POST', 'PUT', 'PATCH'].includes(request.method)) {
+      bodyParser(request, () => route.handler(request, response));
+    } else {
+      route.handler(request, response);
+    }
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+  }
+});
+
+server.listen(3333, () => console.log('Server started at http://localhost:3333'));
---
+++ src/mocks/users.js
@@ -0,0 +1,13 @@
+module.exports = [
+  {
+    id: 1,
+    name: 'Felipe',
+  },
+  {
+    id: 2,
+    name: 'Carina',
+  },{
+    id: 3,
+    name: 'Matheus',
+  },
+]
